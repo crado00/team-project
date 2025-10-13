6f8f57703b8cab3e7de37e608e85166e27654305
crado00 <alrlawowns@gmail.com>
2025-10-13 16:53:29 +0900

    프로젝트 기반 코드

---
+++ music-playlist-backend/src/main/java/com/team10/music_playlist_backend/MusicPlaylistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team10.music_playlist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlaylistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlaylistBackendApplication.class, args);
+	}
+
+}
