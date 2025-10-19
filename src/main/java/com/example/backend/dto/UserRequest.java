4cbe3e8674f8bc6610f3cf3357730bb9d0c9fc7f
박찬혁 <cksgurrhtn@naver.com>
2025-10-19 12:53:16 +0900

    playlist

--- src/main/java/com/example/backend/dto/UserRequest.java
+++ src/main/java/com/example/backend/dto/UserRequest.java
@@ -1,4 +1,28 @@
 package com.example.backend.dto;
 
+import com.example.backend.entity.User;
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
 public class UserRequest {
+    private Long id;
+    private String username;
+    private String email;
+    private String fullName;
+    private String residentialArea;
+    private String selfIntroduction;
+    private String profileImageUrl;
+
+    public static UserRequest from(User u) {
+        return UserRequest.builder()
+                .id(u.getId())
+                .username(u.getUsername())
+                .email(u.getEmail())
+                .fullName(u.getFullName())
+                .residentialArea(u.getResidentialArea())
+                .selfIntroduction(u.getSelfIntroduction())
+                .profileImageUrl(u.getProfileImageUrl())
+                .build();
+    }
 }
