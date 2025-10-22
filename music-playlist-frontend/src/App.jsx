515339198e40d526631ba00476873fac0039c095
crado00 <alrlawowns@gmail.com>
2025-10-22 13:50:24 +0900

    home ui 구성

--- music-playlist-frontend/src/App.jsx
+++ music-playlist-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Profile from "./pages/profile";
+import Home from "./pages/home";
 const App = () => {
 
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
   )
